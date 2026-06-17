2b224baee35a6d63b4cda711b2aed2bc22413e2e
Alberto Oliveira <56661843+OliverallNSB@users.noreply.github.com>
2026-06-16 20:27:04 -0400

    Add Gatekeeper privacy policy and terms pages

---
+++ app/privacy/page.tsx
@@ -0,0 +1,9 @@
+export default function PrivacyPage() {
+  return (
+    <main className="max-w-4xl mx-auto p-6 whitespace-pre-wrap">
+      {`
+PASTE THE ENTIRE PRIVACY POLICY HERE
+`}
+    </main>
+  );
+}
